e8a55f27a2d38032c403b0ef7dd6ab98992f61a9
jjn7 <joel.nottle@gmail.com>
2025-06-10 14:05:00 +0930

    added while loop to continue making calculations

--- calculator.py
+++ calculator.py
@@ -1,5 +1,4 @@
 #create standard definitions of add, subtract, divide and multiply
-
 def add(a, b):
         return a + b
 
@@ -16,26 +15,38 @@ def divide(a, b):
 def multiply(a, b):
         return a * b
 
+#create loop to continue requesting new calculations
+while True: #boolean
+
 #request user input
 #input() collects what the user types - note: input() is always a string in python
-first_input = input("Enter the first number: ")
-second_input = input("Enter the second number: ")
-operation = input("Choose an operation (+, -, /, *): ")
+        first_input = input("Enter the first number: ")
+        second_input = input("Enter the second number: ")
+        operation = input("Choose an operation (+, -, /, *): ")
 
+        try:
 #convert the string to float and assigns it to the variable num1 or num2
-num1 = float(first_input)
-num2 = float(second_input)
+                num1 = float(first_input)
+                num2 = float(second_input)
+        except ValueError: #catch the error if occurs: in this case, not a number
+                print("Invalid input, numbers only.")
+                continue
 
 #use selection construct to determine user input
-if operation == '+':
-        result = add(num1, num2)
-elif operation == '-':
-        result = subtract(num1, num2)
-elif operation == '/':
-        result = divide(num1, num2)
-elif operation == '*':
-        result = multiply(num1, num2)
-else: result = "Invalid operation"
+        if operation == '+':
+                result = add(num1, num2)
+        elif operation == '-':
+                result = subtract(num1, num2)
+        elif operation == '/':
+                result = divide(num1, num2)
+        elif operation == '*':
+                result = multiply(num1, num2)
+        else: result = "Invalid operation"
 
+        print("Result:", result)
 
-print("Result:", result)
+#check if user wants to complete another calculation
+        again = input("Do you want to do another calculation? (y/n): ").lower() #.lower() ensures that the letter is turned into lowercase
+        if again != 'y':
+                print("End of calculation")
+        break
