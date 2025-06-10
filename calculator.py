c269cbebf8f8860647b1867a8d32d977f922fa5f
jjn7 <joel.nottle@gmail.com>
2025-06-10 13:39:33 +0930

    added functions to handle operations

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-#create standard definitions of add, substract, divide and multiply
+#create standard definitions of add, subtract, divide and multiply
 
 def add(a, b):
         return a + b
@@ -12,8 +12,25 @@ def divide(a, b):
 def multiply(a, b):
         return a * b
 
-#requst user input
-#input() collectes what the user types - note: input() is alwasy a string by default
-num1 = input("Enter the first number: ")
-num2 = input("Enter the second number")
+#request user input
+#input() collects what the user types - note: input() is always a string in python
+first_input = input("Enter the first number: ")
+second_input = input("Enter the second number: ")
 operation = input("Choose an operation (+, -, /, *): ")
+
+#convert the string to float and assigns it to the variable num1 or num2
+num1 = float(first_input)
+num2 = float(second_input)
+
+#use selection construct to determine user input
+if operation == '+':
+        result = add(num1, num2)
+elif operation == '-':
+        result = subtract(num1, num2)
+elif operation == '/':
+        result = divide(num1, num2) #divide by zero not yet valid
+elif operation == '*':
+        result = multiply(num1, num2)
+else: result = "Invalid operation"
+
+print("Result:", result)
