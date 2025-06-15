de4d193fad6b9748098e803c02f052d9b6b4e645
jjn7 <jjnottle@gmail.com>
2025-06-15 14:13:31 +0930

    corrected categories syntax

--- PART3/task_3.1_part_3_file_organiser_joel.py
+++ PART3/task_3.1_part_3_file_organiser_joel.py
@@ -13,15 +13,17 @@ else:
 
 #Create a dictionary of file types for sorting
 categories = {
-    "Documents": [".pdf, .doc, .docx, .docm, .txt, .rtf, .csv, .xlsx, .xls, .xps, .xml, .odt, .ott, .ppt, .pptx"],
-    "Images": [".jpg, .jpeg, .bmp, .png, .gif, .exif, .ico, .heif, .heic, .ttif, .tif, .webp"],
-    "Videos": [".3pg, .avi, .mpeg, .m4v, .mov, .webm, .ogg"],
-    "Music": [".mp3, .mp4, .wav, .avif, .raw, .flac, .wma, .ac3, .aac, .midi"],
-    "Archives": [".zip, .rar, .7z, .arc, .cab, .tar"]
+    "Documents": [".pdf", ".doc", ".docx", ".docm", ".txt", ".rtf", ".csv", ".xlsx", ".xls", ".xps", ".xml", ".odt", ".ott", ".ppt", ".pptx"],
+    "Images": [".jpg", ".jpeg", ".bmp", ".png", ".gif", ".exif", ".ico", ".heif", ".heic", ".ttif", ".tif", ".webp"],
+    "Videos": [".3pg", ".avi", ".mpeg", ".m4v", ".mov", ".webm", ".ogg"],
+    "Music": [".mp3", ".mp4", ".wav", ".avif", ".raw", ".flac", ".wma", ".ac3", ".aac", ".midi"],
+    "Archives": [".zip", ".rar", ".7z", ".arc", ".cab", ".tar"]
 }
 
 #retrive list of items
 items = os.listdir(path)
+
+
 
 #create loop through each items
 for item in items:
@@ -31,6 +33,16 @@ for item in items:
     if os.path.isfile(full_path):
         #get file extension
         name, extension = os.path.splitext(item)
+
+        #grant permossion for os to create directories
+        os.makedirs(target_folder, exists_ok=True)
+        #create check for which category the file type belongs to
+        for category, extensions in categories.items():
+            if extension.lower() in extensions:
+                target_folder = os.path.join(path, category)
+                break
+            else:
+                target_folder = os.path.join(path, "Other")
 
 
 
